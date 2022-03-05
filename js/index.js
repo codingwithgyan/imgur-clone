b83a51465bd7e7736ad6494912b28e982060c2f7
Gyaneshwar Kumar <codewithgyan@gmail.com>
2022-03-05 23:01:41 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const CLIENT_ID="0c0c98dd83c6dc7";
+const CLIENT_SECRET="40a709e436678f1993eaef0fc3e878fa63d81f88";
+window.addEventListener("load",()=>{
+    getToken();
+})
+
+
+async function getToken()
+{
+    let res = await fetch(`https://api.imgur.com/3/gallery/top/viral/all/1`,{
+        method:"get",
+        Authorization:`Client-ID ${CLIENT_ID}`
+    })
+    let data = await res.json();
+    console.log(data);
+}
